c085cac7c7cc234a805deb45a854d3ca81985dfb
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-01 18:09:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function createEmployeeRecord(arr){
+    return{ 
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrOfArrs){
+ return arrOfArrs.map(function(info){
+    return createEmployeeRecord(info)
+  })
+}
+
+const TimeInEvent = (obj, date) => {
+
+}
